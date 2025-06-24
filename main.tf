1091405a6fe5ff72670eb36933924bbf1ca239d0
Abhishek <abhishekggsian@gmail.com>
2025-06-24 23:13:37 +0530

    1st

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    azurerm={
+     source  = "hashicorp/azurerm"
+     version = "~>4.34"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "3a734e32-021d-4243-89ff-c3495e6aa4da"
+}
+
+resource "azurerm_resource_group" "rg-sujit" {
+  name     = "rg-sujit"
+  location = "East US"
+  
+}
+resource "azurerm_resource_group" "rg-sujit1" {
+  name     = "rg-sujit1"
+  location = "East US"
+  
+}
+resource "azurerm_resource_group" "rg-sujit2" {
+  name     = "rg-sujit2"
+  location = "East US"
+  
+}
+resource "azurerm_resource_group" "rg-sujit3" {
+  name     = "rg-sujit3"
+  location = "East US"
+  
+}
+
+resource "azurerm_resource_group" "rg-sujit4" {
+  name     = "rg-sujit4"
+  location = "East US"
+  
+}
